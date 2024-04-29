7ef417774d6a9f5bf6d7ccff00ab98f8229543c1
Neil Murphy <barrymun@tcd.ie>
2024-04-29 22:18:00 +0100

    installing react95 and dependencies

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
+import original from "react95/dist/themes/original";
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyles } from "components/global-styles";
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
-  return <>HERE</>;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={original}>HERE</ThemeProvider>
+    </>
+  );
 };
 
 export { App };
