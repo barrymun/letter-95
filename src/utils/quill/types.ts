07d52907ce09b986e64e5c0b41f1b5ed0a6dc873
Neil Murphy <barrymun@tcd.ie>
2024-05-11 21:44:34 +0100

    remove redundant type

--- src/utils/quill/types.ts
+++ src/utils/quill/types.ts
@@ -1,16 +1,3 @@
-// going off older v1.3.7 version: https://github.com/quilljs/quill/blob/v1.3.7/modules/keyboard.js#L129
-export enum Keys137 {
-  BACKSPACE = 8,
-  TAB = 9,
-  ENTER = 13,
-  ESCAPE = 27,
-  LEFT = 37,
-  UP = 38,
-  RIGHT = 39,
-  DOWN = 40,
-  DELETE = 46,
-}
-
 export interface MenuOption {
   value: string;
   label: string;
