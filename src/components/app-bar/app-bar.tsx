5df612fad97418b6b0e791d3deb559d68b6f006e
Neil Murphy <barrymun@tcd.ie>
2024-05-12 22:28:33 +0100

    some work on app-bar but need to consolidate styles

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,14 +1,35 @@
 import "./app-bar.scss";
 
-import { FC } from "react";
-import { AppBar as R95AppBar } from "react95";
+import { FC, useState } from "react";
+import { Button, MenuList, MenuListItem, AppBar as R95AppBar, Toolbar } from "react95";
 
 interface AppBarProps {}
 
 const AppBar: FC<AppBarProps> = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div className="app-bar">
-      <R95AppBar>Here</R95AppBar>
+      <R95AppBar position="fixed">
+        <Toolbar>
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <Button active={isOpen} onClick={() => setIsOpen(!isOpen)}>
+              File
+            </Button>
+            {isOpen && (
+              <MenuList
+                style={{
+                  position: "absolute",
+                  top: "100%",
+                  left: 0,
+                }}
+              >
+                <MenuListItem>Save</MenuListItem>
+              </MenuList>
+            )}
+          </div>
+        </Toolbar>
+      </R95AppBar>
     </div>
   );
 };
