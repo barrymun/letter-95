3c145cdddefc4d8a4f1a54c1c126f396b9b47266
Neil Murphy <barrymun@tcd.ie>
2024-06-17 11:26:20 +0100

    improved code splitting

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,13 @@
+import emojiData from "@emoji-mart/data";
+import { init } from "emoji-mart";
 import { FC, Suspense, lazy } from "react";
 
 import { AppBar } from "components/app-bar";
 import { Container, Layout } from "components/layout";
 
 const RichTextEditor = lazy(() => import("components/rich-text-editor/rich-text-editor"));
+
+init({ data: emojiData });
 
 interface AppProps {}
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import "utils/quill/modules/menu/menu.scss";
 import "utils/quill/modules/mention/mention.scss";
 import "utils/quill/modules/custom-emoji-mart/custom-emoji-mart.scss";
 
-import emojiData from "@emoji-mart/data";
-import { init } from "emoji-mart";
-import React, { Suspense, lazy } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import original from "react95/dist/themes/original";
 import { ThemeProvider } from "styled-components";
@@ -14,10 +12,8 @@ import { ThemeProvider } from "styled-components";
 const App = lazy(() => import("components/app"));
 const GlobalStyles = lazy(() => import("components/global-styles"));
 
-init({ data: emojiData });
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <Suspense>
       <GlobalStyles />
     </Suspense>
@@ -26,5 +22,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </Suspense>
     </ThemeProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );
