1efc01f1e9e924c5b66e05f3a15e17594e55e6d1
Neil Murphy <barrymun@tcd.ie>
2024-06-18 20:51:51 +0100

    working download as pdf

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,5 +1,7 @@
 import "./app-bar.scss";
 
+import html2canvas from "html2canvas";
+import { jsPDF as JSPDF } from "jspdf";
 import { FC, useState } from "react";
 import { Button, MenuList, MenuListItem, AppBar as R95AppBar, Toolbar } from "react95";
 
@@ -8,7 +10,19 @@ interface AppBarProps {}
 const AppBar: FC<AppBarProps> = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const handleSave = () => {
+  const handleSave = async () => {
+    const element = document.querySelector(".ql-editor");
+    if (!element) {
+      return;
+    }
+    const canvas = await html2canvas(element as HTMLElement);
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new JSPDF();
+    const imgProps = pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("document.pdf");
     setIsOpen(false);
   };
 
