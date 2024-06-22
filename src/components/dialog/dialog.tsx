986d2f794818a30c7241d2287a19b87ca2687e50
Neil Murphy <barrymun@tcd.ie>
2024-06-22 08:24:14 +0100

    draggable dialog

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -5,6 +5,10 @@ import { ScrollView, Window, WindowContent } from "react95";
 
 import { CloseButton } from "components/presentational";
 
+let canDrag = false;
+let topOffset = 0;
+let leftOffset = 0;
+
 interface DialogProps {
   show: boolean;
   setShow: (_: boolean) => void;
@@ -16,6 +20,7 @@ const Dialog: FC<DialogProps> = (props) => {
   const { show, setShow, title, children } = props;
 
   const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const dialogWindowRef = useRef<HTMLDivElement | null>(null);
 
   const showDialog = () => {
     if (!dialogRef.current) {
@@ -39,11 +44,43 @@ const Dialog: FC<DialogProps> = (props) => {
     }
   };
 
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    // ignore right click
+    if (e.button === 2) {
+      return;
+    }
+    canDrag = true;
+    if (dialogWindowRef.current) {
+      const rect = dialogWindowRef.current.getBoundingClientRect();
+      topOffset = rect.top - e.clientY;
+      leftOffset = rect.left - e.clientX;
+    }
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!canDrag) {
+      return;
+    }
+    const x = e.clientX;
+    const y = e.clientY;
+    if (dialogWindowRef.current) {
+      dialogWindowRef.current.style.left = `${x + leftOffset}px`;
+      dialogWindowRef.current.style.top = `${y + topOffset}px`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    canDrag = false;
+    topOffset = 0;
+    leftOffset = 0;
+  };
+
   useEffect(() => {
     if (show) {
       showDialog();
     } else {
       hideDialog();
+      dialogWindowRef.current?.removeAttribute("style");
     }
   }, [show]);
 
@@ -57,12 +94,29 @@ const Dialog: FC<DialogProps> = (props) => {
     };
   }, []);
 
+  /**
+   * user can drag the dialog (window) anywhere on the page
+   */
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
   return (
     <dialog className="dialog" ref={dialogRef}>
-      <Window>
-        <WindowContent>
+      <Window className="window" ref={dialogWindowRef}>
+        <WindowContent className="window-content">
           <div className="root">
-            <div className="title">
+            <div className="title" role="presentation" onMouseDown={handleMouseDown} onContextMenu={() => false}>
               {title}
               <CloseButton onClick={hideDialog} />
             </div>
