72fe835151e9e19e70172f68c822e41a9dfa5443
Neil Murphy <barrymun@tcd.ie>
2024-06-23 14:59:08 +0100

    big rework for the buttons and toolbar (using r95 icons), update cspell words

--- src/components/rich-text-editor/custom-toolbar.tsx
+++ src/components/rich-text-editor/custom-toolbar.tsx
@@ -1,23 +1,70 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import "./custom-toolbar.scss";
 
+import {
+  ArrowLeft,
+  ArrowRight,
+  Bold,
+  Brush,
+  Centre,
+  Drvspace7,
+  Font,
+  Font2,
+  Inetcpl1318,
+  Italic,
+  Justify,
+  Left,
+  ParaBul,
+  ParaNum,
+  Progman24,
+  Right,
+  Underlne,
+  WebLink,
+} from "@react95/icons";
+import Quill from "quill/core";
 import { ForwardedRef, forwardRef } from "react";
+import { renderToString } from "react-dom/server";
 
 import { Emoji } from "components/svgs";
+import { useTheme } from "hooks";
+import { SvgProps } from "utils";
+
+const buttonProps: SvgProps = {
+  width: 18,
+  height: 18,
+};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const icons = Quill.import("ui/icons") as Record<string, any>;
+icons.header["1"] = renderToString(<Font {...buttonProps} />);
+icons.header["2"] = renderToString(<Font2 {...buttonProps} />);
+icons.bold = renderToString(<Bold {...buttonProps} />);
+icons.italic = renderToString(<Italic {...buttonProps} />);
+icons.underline = renderToString(<Underlne {...buttonProps} />);
+// icons.strike = renderToString(<Strike {...buttonProps} />); // does not exist
+icons.indent["-1"] = renderToString(<ArrowLeft {...buttonProps} />);
+icons.indent["+1"] = renderToString(<ArrowRight {...buttonProps} />);
+icons.align[""] = renderToString(<Left {...buttonProps} />);
+icons.align.center = renderToString(<Centre {...buttonProps} />);
+icons.align.right = renderToString(<Right {...buttonProps} />);
+icons.align.justify = renderToString(<Justify {...buttonProps} />);
+icons.list.ordered = renderToString(<ParaNum {...buttonProps} />);
+icons.list.bullet = renderToString(<ParaBul {...buttonProps} />);
+icons.background = renderToString(<Brush {...buttonProps} />);
+icons.color = renderToString(<Inetcpl1318 {...buttonProps} />);
+icons.image = renderToString(<Progman24 {...buttonProps} />);
+icons.link = renderToString(<WebLink {...buttonProps} />);
+icons.emoji = renderToString(<Drvspace7 {...buttonProps} />);
 
 interface CustomToolbarProps {}
 
 const CustomToolbar = forwardRef((_props: CustomToolbarProps, ref: ForwardedRef<HTMLDivElement>) => {
+  const { theme } = useTheme();
+
   return (
-    <div ref={ref} className="custom-toolbar">
+    <div ref={ref} className="custom-toolbar" style={{ backgroundColor: theme.material }}>
       <span className="ql-formats">
         <button type="button" className="ql-header" value="1" />
         <button type="button" className="ql-header" value="2" />
-        {/* the following require custom icons */}
-        {/* <button type="button" className="ql-header" value="3" /> */}
-        {/* <button type="button" className="ql-header" value="4" /> */}
-        {/* <button type="button" className="ql-header" value="5" /> */}
-        {/* <button type="button" className="ql-header" value="6" /> */}
       </span>
       <span className="ql-formats">
         <button type="button" className="ql-bold" />
@@ -39,7 +86,6 @@ const CustomToolbar = forwardRef((_props: CustomToolbarProps, ref: ForwardedRef<
         <button type="button" className="ql-list" value="ordered" />
         <button type="button" className="ql-list" value="bullet" />
       </span>
-      {/* TODO: */}
       <span className="ql-formats">
         <select className="ql-background" />
         <select className="ql-color" />
