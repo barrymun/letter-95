9a9a4ea82cc9885b0c4a156bb8a4d20022db9cb5
Neil Murphy <barrymun@tcd.ie>
2024-06-29 19:04:56 +0100

    refactor css imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import "./index.scss";
 import "quill/dist/quill.snow.css";
 import "utils/quill/modules/menu/menu.scss";
 import "utils/quill/modules/mention/mention.scss";
 import "utils/quill/modules/custom-emoji-mart/custom-emoji-mart.scss";
+import "index.scss";
 import "i18n/config";
 
 import { StrictMode, Suspense, lazy } from "react";
